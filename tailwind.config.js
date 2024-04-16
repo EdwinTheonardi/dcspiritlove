89b7b1eb48fcab748fe12aa2ed128018cee66a29
EdwinTheonardi <edwintheonardi88@gmail.com>
2024-04-16 14:30:00 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        "inter" : ['inter']
+      },
+      colors : {
+
+      }
+    },
+
+  },
+  plugins: [],
+}
+
